04560dd3d68155ce79e5776a8827edd91ad9ffef
waayo69 <kristianjose123@gmail.com>
2025-04-03 17:41:32 +0800

    ambot

---
+++ MVVM/Data/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JoseNi.MVVM.Models;
+
+namespace JoseNi.MVVM.Data
+{
+    public static class Users
+    {
+        public static List<AppModel> RegisteredNiggers { get; set; } = new List<AppModel>();
+    }
+}
---
+++ MVVM/ViewModels/AppViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoseNi.MVVM.ViewModels
+{
+    public class AppViewModel
+    {
+        // Property to store the username
+        public string Username { get; set; }
+
+        // Property to store the password
+        public string Password { get; set; }
+
+
+    }
+}
